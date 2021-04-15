8791fc7441281bdc53b2e1954fa57af7e7650686
yantao <yt7589@qq.com>
2021-04-15 16:26:18 +0800

    add message service entry point

--- api/message_service.py
+++ api/message_service.py
@@ -3,3 +3,24 @@
 class MessageService(object):
     def __init__(self):
         self.name = 'api.MessageService'
+
+    def startup(self):
+        print('启动Thrift消息服务器')
+
+    def send_sms(self, mobile_phone, msg):
+        """
+        发送短信
+        Parameters:
+         - mobile_phone 手机号
+         - msg 消息内容
+        """
+        print('向{0}发送消息：{1};'.format(mobile_phone, msg))
+
+    def send_email(self, email_addr, msg):
+        """
+        发送邮件
+        Parameters:
+         - email_addr 邮箱地址
+         - msg 消息内容
+        """
+        print('向{0}发送邮件：{1};'.format(email_addr, msg))
--- app_main.py
+++ app_main.py
@@ -1,7 +1,10 @@
 # 主程序入口
+from api.message_service import MessageService
 
 def main(args={}):
     print('aisc-tms-message v0.0.1')
+    message_service = MessageService()
+    message_service.startup()
 
 if '__main__' == __name__:
     main()
